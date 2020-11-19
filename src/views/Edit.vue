f3153e25c2b23ca25d45721dc0431bc0b7321e4b
yj <1026252616@qq.com>
2020-11-19 20:13:19 +0800

    编辑资料页面使用条形组件

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -11,40 +11,26 @@
         </div>
       </div>
     </div>
-    <ul class="editUserInfo">
-      <li class="userName">
-        <p>
-          昵称
-          <i>牛</i>
-          <span class="iconfont iconjiantou1"></span>
-        </p>
-      </li>
-      <li class="userPwd">
-        <p>
-          密码
-          <i>******</i>
-          <span class="iconfont iconjiantou1"></span>
-        </p>
-      </li>
-      <li class="userGender">
-        <p>
-          性别
-          <i>男</i>
-          <span class="iconfont iconjiantou1"></span>
-        </p>
-      </li>
-    </ul>
+
+    <Info text="昵称" message="牛"></Info>
+    <Info text="密码" message="******"></Info>
+    <Info text="性别" message="男"></Info>
   </div>
 </template>
 
 <script>
-export default {};
+import Info from "../components/Info";
+export default {
+  components: {
+    Info,
+  },
+};
 </script>
 
 <style lang="less" scoped>
 .container {
-  padding: 15/360 * 100vw 0 0 20/360 * 100vw;
   .edit {
+    padding: 15/360 * 100vw 0 0 20/360 * 100vw;
     .editUserImg {
       p {
         position: relative;
@@ -63,39 +49,11 @@ export default {};
       }
     }
   }
-  .editUserInfo {
-    li {
-      border-bottom: 2/360 * 100vw solid #e4e4e4;
-      padding: 10/360 * 100vw 0;
-      p {
-        position: relative;
-        font-family: "Roboto Regular", "Roboto";
-        font-weight: 400;
-        font-style: normal;
-        font-size: 14/360 * 100vw;
-        color: #333333;
-        i {
-          position: absolute;
-          right: 30/360 * 100vw;
-          font-style: normal;
-          font-size: 13/360 * 100vw;
-          color: rgba(148, 148, 148, 0.866666666666667);
-        }
-      }
-    }
-  }
 }
 .iconjiantou2 {
   position: absolute;
   left: 0;
   font-size: 15/360 * 100vw;
   color: rgb(180, 187, 197);
-}
-.iconjiantou1 {
-  position: absolute;
-  right: 10/360 * 100vw;
-  //   top: 10/360 * 100vw;
-  font-size: 17/360 * 100vw;
-  color: rgba(148, 148, 148, 0.866666666666667);
 }
 </style>
