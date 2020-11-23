b140008c249f6373529b61aac78bfa1d2bbc03c2
yj <1026252616@qq.com>
2020-11-23 15:13:49 +0800

    获取关注列表

--- src/views/profile/Follow.vue
+++ src/views/profile/Follow.vue
@@ -1,25 +1,43 @@
 <template>
   <div class="container">
-    <TopNav title="我的关注" @click.native="backUser" />
-    <MyFollow />
-    <MyFollow />
-    <MyFollow />
+    <TopNav title="我的关注" @click.native="$router.back(-1)" />
+    <div class="fence" v-for="item in followList" :key="item.id">
+      <img
+        v-if="item.head_img"
+        :src="$axios.defaults.baseURL + item.head_img"
+        alt=""
+      />
+      <img v-else src="@/assets/1.png" alt="" class="newsImg" />
+      <div class="info">
+        <div class="name">{{ item.name }}</div>
+        <div class="date">2020-11-20</div>
+      </div>
+      <button class="unfollow">取消关注</button>
+    </div>
   </div>
 </template>
 
 <script>
-import MyFollow from "@/components/MyFollow";
 import TopNav from "@/components/TopNav";
 export default {
   components: {
-    MyFollow,
     TopNav,
   },
-  methods: {
-    backUser() {
-      this.$router.push("/user");
-    },
+  data() {
+    return {
+      followList: [],
+    };
   },
+  created() {
+    // 获取关注数据
+    this.$axios({
+      url: "user_follows",
+    }).then((res) => {
+      console.log(res);
+      this.followList = res.data.data;
+    });
+  },
+  methods: {},
 };
 </script>
 
@@ -27,5 +45,43 @@ export default {
 .container {
   display: flex;
   flex-direction: column;
+  .fence {
+    display: flex;
+    align-items: center;
+    border-bottom: 2px solid#d7d7d7;
+    padding: 15/360 * 100vw;
+    padding-left: 0;
+
+    .newsImg {
+      width: 40/360 * 100vw;
+      height: 40/360 * 100vw;
+      border-radius: 50%;
+      margin-left: 20/360 * 100vw;
+    }
+    .info {
+      flex: 1;
+      margin-left: 10/360 * 100vw;
+
+      .name {
+        font-size: 16 /360 * 100vw;
+        color: #333;
+      }
+      .date {
+        color: #888;
+        font-size: 14/360 * 100vw;
+      }
+    }
+
+    .unfollow {
+      width: 73/360 * 100vw;
+      height: 30/360 * 100vw;
+      line-height: 30/360 * 100vw;
+      border-radius: 50/360 * 100vw;
+      background-color: rgba(153, 153, 153, 0.2);
+      text-align: center;
+      font-size: 12/360 * 100vw;
+      border: 0;
+    }
+  }
 }
 </style>
