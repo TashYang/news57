64860a24b1fa8dc5b8fb70c37cb47b38ef6ba81c
yj <1026252616@qq.com>
2020-11-25 16:27:09 +0800

    手动添加收藏

---
+++ src/views/test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="addCollect(68)">添加收藏</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    addCollect(id) {
+      this.$axios({
+        url: "/post_star/" + id,
+      }).then((res) => {
+        console.log(res);
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
