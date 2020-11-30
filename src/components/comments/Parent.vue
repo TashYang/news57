07b3ecae3d28a647fd378be6832da768c56136a8
yj <1026252616@qq.com>
2020-11-30 16:05:52 +0800

    优化页面布局

--- src/components/comments/Parent.vue
+++ src/components/comments/Parent.vue
@@ -2,7 +2,7 @@
   <div class="parentBox">
     <Parent v-if="parentData.parent" :parentData="parentData.parent" />
     <div class="info">
-      <div class="name">火星网友</div>
+      <div class="name">{{ parentData.user.nickname }}</div>
       <div class="date">2小时前</div>
       <div class="reply">回复</div>
     </div>
@@ -26,22 +26,26 @@ export default {
   .info {
     display: flex;
     align-items: center;
-    margin-bottom: 10/360 * 100vw;
-    padding-left: 10/360 * 100vw;
+    font-size: 16 /360 * 100vw;
     color: #333;
-    font-size: 14/360 * 100vw;
+    margin-bottom: 10 /360 * 100vw;
 
     .date {
       flex-grow: 1;
-      padding-left: 5/360 * 100vw;
-      font-size: 12/360 * 100vw;
       color: #888;
+      font-size: 14 /360 * 100vw;
+      padding-left: 10 /360 * 100vw;
     }
 
     .reply {
       font-size: 12 /360 * 100vw;
       color: #888;
     }
+  }
+  .content {
+    font-size: 16 /360 * 100vw;
+    padding: 10 /360 * 100vw 0;
+    color: #666;
   }
 }
 </style>
