a7da828244b9aeaf28a74345370cb9b433443e5f
yj <1026252616@qq.com>
2020-12-05 20:22:59 +0800

    封装axios

--- src/main.js
+++ src/main.js
@@ -26,12 +26,10 @@ import 'vant/lib/action-sheet/style'
 import 'vant/lib/toast/style'
 
 // 引入axios
-import axios from 'axios'
+import axios from './utils/axios'
 // 原型绑定 axios
 Vue.prototype.$axios = axios
 
-// 设置基准路径
-axios.defaults.baseURL = "http://157.122.54.189:9083"
 
 // 设置全局过滤器,如果获取到的数据图片路径不完整则补全
 Vue.filter('fixImgUrl', (oldUrl) => {
@@ -42,31 +40,9 @@ Vue.filter('fixImgUrl', (oldUrl) => {
   }
 })
 
-// 设置请求拦截器
-axios.interceptors.request.use(config => {
-  if (!config.headers.Authorization && localStorage.getItem('token')) {
-    config.headers.Authorization = localStorage.getItem('token')
-  };
-  return config;
 
-});
 
-// 设置拦截器同意拦截
-axios.interceptors.response.use(res => {
-  const errCodePattern = /^4\d{2}$/
-  // 如果没有token就登入的话显示错误信息
-  if (errCodePattern.test(res.data.statusCode)) {
-    Toast.fail(res.data.message || "系统错误")
-  }
-  // 如果后台捕获不到token的话就跳回登录页
-  if (res.data.message === "用户信息验证失败") {
-    localStorage.removeItem("token");
-    localStorage.removeItem("userId");
-    router.replace("/login");
-  }
 
-  return res;
-});
 Vue.config.productionTip = false
 
 
